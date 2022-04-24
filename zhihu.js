ae1d51ed6716e995b1c4de16a398d6cf8694e43c
幻非 <eg1981400335@gmail.com>
2022-04-25 00:01:54 +0800

    Update zhihu.js

--- zhihu.js
+++ zhihu.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎宽屏
 // @namespace    https://www.cnblogs.com/huanfeiiiii/
-// @version      0.3
+// @version      0.4
 // @description  将网页主体部分变宽，去除杂冗部分
 // @author       huanfei
 // @match        https://www.zhihu.com/*
@@ -38,6 +38,29 @@
         observe("AnswerFormPortalContainer", question);
     }
 
+    function collection() {
+        // 收藏夹页
+        var body = document.querySelector("#root > div > main > div > div.CollectionsDetailPage-mainColumn");
+        body.setAttribute("style", "width: 100%");
+
+        window.onload = function () {
+            var side = document.querySelector("#root > div > main > div > div.CollectionDetailPageSideBar > div");
+            side.style.position = 'absolute';
+            var height = getComputedStyle(side, null).height;
+            var width = getComputedStyle(side, null).width;
+            side.setAttribute("style", "height:50px;width:100px;position: absolute;right:0;transition:all 0.5s")
+
+            side.onmouseover = function () {
+                side.style.height = height
+                side.style.width = width
+            }
+            side.onmouseout = function () {
+                side.style.height = '50px'
+                side.style.width = '100px'
+            }
+        }
+    }
+
     function observe(id, fun) {
         //对传入ID的元素进行检测，如果发生变化就执行函数
 
@@ -45,20 +68,20 @@
 
         // 观察者的选项(要观察哪些突变)
         var config = { attributes: true, childList: true, subtree: true };
-    
+
         // 当观察到突变时执行的回调函数
-        var callback = function(mutationsList) {
-            mutationsList.forEach(function(item,index){
+        var callback = function (mutationsList) {
+            mutationsList.forEach(function (item, index) {
                 fun();
             });
         };
-    
+
         // 创建一个链接到回调函数的观察者实例
         var observer = new MutationObserver(callback);
-    
+
         // 开始观察已配置突变的目标节点
         observer.observe(targetNode, config);
-    
+
         // 停止观察
         // observer.disconnect();
     }
@@ -69,6 +92,8 @@
         searchPage();
     } else if (url.search("question") != -1) {
         question();
+    } else if (url.search("collection") != -1) {
+        collection();
     }
 
 })();
