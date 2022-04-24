a81a2cc58339dd5e48919ee6b1bf25f616c3dd29
幻非 <eg1981400335@gmail.com>
2022-04-24 18:47:32 +0800

    0.2

--- zhihu.js
+++ zhihu.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎宽屏
 // @namespace    https://www.cnblogs.com/huanfeiiiii/
-// @version      0.1
+// @version      0.2
 // @description  将网页主体部分变宽，去除杂冗部分
 // @author       huanfei
 // @match        https://www.zhihu.com/*
@@ -12,11 +12,13 @@
 (function () {
     var url = window.location.href;
 
-    function homePage() {
+    function homePage() {   
         // 主页
         var body = document.querySelector("#root > div > main > div > div.Topstory-container > div")
         body.setAttribute("style", "width:100%")
-        document.querySelector("#root > div > main > div > div.Topstory-container > div.GlobalSideBar").setAttribute("style", "display:none")
+
+        var side = document.querySelector("#root > div > main > div > div.Topstory-container > div.GlobalSideBar")
+        side.setAttribute("style", "display:none")
     }
 
     function searchPage() {
@@ -24,10 +26,22 @@
         document.querySelector("#SearchMain").setAttribute("style", "width:100%")
     }
 
+    function question() {
+        // 问题页
+
+        var side = document.querySelector("#root > div > main > div > div > div.Question-main > div.Question-sideColumn.Question-sideColumn--sticky")
+        side.setAttribute("style", "display:none")
+
+        var body = document.querySelector("#root > div > main > div > div > div.Question-main > div.ListShortcut > div")
+        body.setAttribute("style", "width:100%")
+    }
+
     if (url == "https://www.zhihu.com/") {
         homePage();
     } else if (url.search("search") != -1) {
         searchPage()
+    } else if (url.search("question") != -1) {
+        question()
     }
 
 })();
