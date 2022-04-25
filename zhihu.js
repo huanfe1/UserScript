14b3cb51f849c799a23a110b200b95b0007446b7
幻非 <eg1981400335@gmail.com>
2022-04-25 23:23:25 +0800

    update

--- zhihu.js
+++ zhihu.js
@@ -10,28 +10,35 @@
 // @license      MIT
 // ==/UserScript==
 
+
+
 (function () {
 
     function homePage() {
         // 主页
-        var body = document.querySelector("#root > div > main > div > div.Topstory-container > div");
-        body.setAttribute("style", "width:100%");
 
+        // 主体
+        var body = document.querySelector("#root > div > main > div > div.Topstory-container > div");
+        // 侧边栏
         var side = document.querySelector("#root > div > main > div > div.Topstory-container > div.GlobalSideBar");
         var height = getComputedStyle(side, null).height
-        side.style.position = "absolute";
+        
+        body.setAttribute("style", "width:100%");
 
-
-        var SVG_NS = "http://www.w3.org/2000/svg"
-        var svgArea = document.querySelector("#root > div > main > div > div.Topstory-container > div.GlobalSideBar > div");
+        // 清除文字部分
+        var footer = document.querySelector("#root > div > main > div > div.Topstory-container > div.GlobalSideBar > div > div > footer");
+        footer.remove();
+        
 
         // 创建svg
-        var tag = document.createElementNS(SVG_NS, 'svg')
+        var SVG_NS = "http://www.w3.org/2000/svg";
+
+        var tag = document.createElementNS(SVG_NS, 'svg');
         var path = document.createElementNS(SVG_NS, "path");
 
         // 设置属性
         tag.setAttribute("width", "100%");
-        tag.setAttribute("height", "50px");
+        tag.setAttribute("height", "30px");
         tag.setAttribute("viewBox", "0 0 24 24");
         tag.setAttribute("data-new-api", "GearFill24");
         tag.setAttribute("data-old-api", "Settings");
@@ -41,20 +48,24 @@
         path.setAttribute('d', "M10.809 2.17a2.415 2.415 0 012.382 0l6.842 3.878a2.415 2.415 0 011.224 2.1v7.703c0 .87-.468 1.672-1.224 2.101L13.19 21.83a2.415 2.415 0 01-2.381 0l-6.842-3.878a2.415 2.415 0 01-1.224-2.1V8.148c0-.87.467-1.672 1.224-2.101l6.842-3.878zm-2.633 9.82a3.858 3.858 0 117.716 0 3.858 3.858 0 01-7.716 0zm3.858-2.357a2.358 2.358 0 100 4.716 2.358 2.358 0 000-4.716z");
         path.setAttribute("fill-rule", "evenodd");
         path.setAttribute("clip-rule", "evenodd");
-        
+
         // 添加到页面
         tag.appendChild(path);
-        svgArea.prepend(tag)
-    
+        side.prepend(tag);
+
         // 侧边栏缩小
-        side.setAttribute("style", "overflow:hidden;width:300px;position:absolute;height:50px;right:-20%;transition:all 0.5s");
+        
+        side.style.position = "absolute";
+        side.setAttribute("style", "overflow:hidden;width:30px;position:absolute;height:30px;right:-20%;transition:all 0.5s");
 
         // 侧栏伸缩鼠标操作
         side.onmouseover = function () {
             side.style.height = height;
+            side.style.width = "300px";
         }
         side.onmouseout = function () {
-            side.style.height = '50px';
+            side.style.height = '30px';
+            side.style.width = '30px';
         }
 
         // 解决页面滑动问题
@@ -74,7 +85,7 @@
     }
 
 
-    function question() {
+    function questionPage() {
         // 问题页
         var side = document.querySelector("#root > div > main > div > div > div.Question-main > div.Question-sideColumn.Question-sideColumn--sticky");
         side.setAttribute("style", "display:none");
@@ -82,11 +93,11 @@
         var body = document.querySelector(".Question-mainColumn");
         body.setAttribute("style", "width:100%");
 
-        observe(document.getElementById("AnswerFormPortalContainer"), question);
+        observe(document.getElementById("AnswerFormPortalContainer"), questionPage);
     }
 
 
-    function collection() {
+    function collectionPage() {
         // 收藏夹页
         var body = document.querySelector("#root > div > main > div > div.CollectionsDetailPage-mainColumn");
         body.setAttribute("style", "width: 100%");
@@ -96,22 +107,27 @@
             side.style.position = 'fixed';
             var height = getComputedStyle(side, null).height;
             var width = getComputedStyle(side, null).width;
-            side.setAttribute("style", "height:50px;width:100px;position: fixed;right:0;top:10%;transition:all 0.5s")
+            side.setAttribute("style", "height:50px;width:100px;position: fixed;right:10px;top:10%;transition:all 0.5s")
 
             side.onmouseover = function () {
-                side.style.height = height
-                side.style.width = width
+                side.style.height = height;
+                side.style.width = width;
             }
             side.onmouseout = function () {
-                side.style.height = '50px'
-                side.style.width = '100px'
+                side.style.height = '50px';
+                side.style.width = '100px';
             }
         }
     }
 
 
-    function observe(element, fun) {
-        //对传入ID的元素进行检测，如果发生变化就执行函数
+    function peoplePage() {
+        // TODO个人页面
+    }
+
+
+    function observe(element, func) {
+        //对传入的元素进行检测，如果发生变化就执行函数
 
         var targetNode = element;
 
@@ -121,13 +137,7 @@
         // 当观察到突变时执行的回调函数
         var callback = function (mutationsList) {
             mutationsList.forEach(function (item, index) {
-                fun()
-                // if (item.type == 'childList') {
-                //     console.log('有节点发生改变，当前节点的内容是：');
-                //     console.log(item.target.innerHTML);
-                // } else if (item.type == 'attributes') {
-                //     console.log('修改了'+item.attributeName+'属性');
-                // }
+                func();
             });
         };
 
@@ -143,16 +153,18 @@
 
     var url = window.location.href;
 
-    if (url == "https://www.zhihu.com/") {
+    if (url.search("https://www.zhihu.com|zvideo|follow|hot|") != -1) {
         homePage();
     } else if (url.search("search") != -1) {
         searchPage();
     } else if (url.search("question") != -1) {
-        question();
+        questionPage();
         console.log("问题页")
     } else if (url.search("collection/[0-9]+") != -1) {
-        collection();
+        collectionPage();
         console.log("收藏页")
+    } else if (url.search("people") != -1) {
+        peoplePage();
     }
 
 })();
