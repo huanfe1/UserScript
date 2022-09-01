67e197871538f78b04bab4d43422ab03015bb91e
幻非 <eg1981400335@gmail.com>
2022-09-01 21:20:20 +0800

    更新添加推荐方法，更新去除专栏小尾巴

--- bilibiliSimplified/bilibiliSimplified.user.js
+++ bilibiliSimplified/bilibiliSimplified.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         B站简化
 // @namespace    https://github.com/huanfeiiiii/UserScript/tree/main/bilibiliSimplified
-// @version      0.6.2
+// @version      0.6.3
 // @description  简化B站
 // @author       huanfei
 // @match        *.bilibili.com/*
@@ -96,10 +96,8 @@
 
     function readPage() {
         // 专栏去除复制小尾巴
-        HTMLDivElement.prototype.realAddEventListener = HTMLAnchorElement.prototype.addEventListener;
-        HTMLDivElement.prototype.addEventListener = function (a, b, c) {
-            if (a == "copy") return;
-            return this.realAddEventListener(a, b, c);
+        window.onload = () => {
+            document.getElementById("article-content").parentNode.innerHTML += "";
         };
     }
 
@@ -188,34 +186,64 @@
     function addRecommend() {
         // 添加推荐视频
         let data = recommendData.pop();
-        let url = `https://www.bilibili.com${data.goto == "av" ? `/video/av${data.param}` : data.uri}`;
-        let text = `
-        <a href="${url}" class="video_content" target="_blank"><div class="preview_pic"><div class="preview_bar"><div class="bar_content"></div></div></div></div><img src="${
-            data.cover
-        }@672w_378h" alt="${
-            data.title
-        }"><div class="video_mark"><div class="video_mark_text"></div></div><div class="video_mask"><div class="views"><svg t="1658670007102" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="18155" width="200" height="200"><path d="M800 128H224C134.4 128 64 198.4 64 288v448c0 89.6 70.4 160 160 160h576c89.6 0 160-70.4 160-160V288c0-89.6-70.4-160-160-160z m96 608c0 54.4-41.6 96-96 96H224c-54.4 0-96-41.6-96-96V288c0-54.4 41.6-96 96-96h576c54.4 0 96 41.6 96 96v448z" p-id="18156" fill="#ffffff"></path><path d="M684.8 483.2l-256-112c-22.4-9.6-44.8 6.4-44.8 28.8v224c0 22.4 22.4 38.4 44.8 28.8l256-112c25.6-9.6 25.6-48 0-57.6z" p-id="18157" fill="#ffffff"></path></svg><p class="num">                ${numConverter.view(
-            data.play
-        )}</p><svg t="1658672093240" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3299" width="200" height="200"><path d="M857.28 344.992h-264.832c12.576-44.256 18.944-83.584 18.944-118.208 0-78.56-71.808-153.792-140.544-143.808-60.608 8.8-89.536 59.904-89.536 125.536v59.296c0 76.064-58.208 140.928-132.224 148.064l-117.728-0.192A67.36 67.36 0 0 0 64 483.04V872c0 37.216 30.144 67.36 67.36 67.36h652.192a102.72 102.72 0 0 0 100.928-83.584l73.728-388.96a102.72 102.72 0 0 0-100.928-121.824zM128 872V483.04c0-1.856 1.504-3.36 3.36-3.36H208v395.68H131.36A3.36 3.36 0 0 1 128 872z m767.328-417.088l-73.728 388.96a38.72 38.72 0 0 1-38.048 31.488H272V476.864a213.312 213.312 0 0 0 173.312-209.088V208.512c0-37.568 12.064-58.912 34.72-62.176 27.04-3.936 67.36 38.336 67.36 80.48 0 37.312-9.504 84-28.864 139.712a32 32 0 0 0 30.24 42.496h308.512a38.72 38.72 0 0 1 38.048 45.888z" p-id="3300" fill="#ffffff"></path></svg><p class="num">                ${numConverter.view(
-            data.like
-        )}</p></div><div class="duration">            ${numConverter.duration(
-            data.duration
-        )}</div></div></a><div class="video_text"><a class="video_title" href="${url}" target="_blank" title="${data.title}">${
-            data.title
-        }</a><div class="owner"><a class="up_name" href="https://space.bilibili.com/${
-            data.mid
-        }" target="_blank"><svg width="18" height="16" viewBox="0 0 18 16" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M9 2.0625C6.85812 2.0625 4.98983 2.1725 3.67735 2.2798C2.77861 2.35327 2.08174 3.04067 2.00119 3.93221C1.90388 5.00924 1.8125 6.43727 1.8125 8C1.8125 9.56273 1.90388 10.9908 2.00119 12.0678C2.08174 12.9593 2.77861 13.6467 3.67735 13.7202C4.98983 13.8275 6.85812 13.9375 9 13.9375C11.1421 13.9375 13.0105 13.8275 14.323 13.7202C15.2216 13.6467 15.9184 12.9595 15.9989 12.0682C16.0962 10.9916 16.1875 9.56386 16.1875 8C16.1875 6.43614 16.0962 5.00837 15.9989 3.9318C15.9184 3.04049 15.2216 2.3533 14.323 2.27983C13.0105 2.17252 11.1421 2.0625 9 2.0625ZM3.5755 1.03395C4.9136 0.924562 6.81674 0.8125 9 0.8125C11.1835 0.8125 13.0868 0.924583 14.4249 1.03398C15.9228 1.15645 17.108 2.31588 17.2438 3.81931C17.3435 4.92296 17.4375 6.38948 17.4375 8C17.4375 9.61052 17.3435 11.077 17.2438 12.1807C17.108 13.6841 15.9228 14.8436 14.4249 14.966C13.0868 15.0754 11.1835 15.1875 9 15.1875C6.81674 15.1875 4.9136 15.0754 3.5755 14.966C2.07738 14.8436 0.892104 13.6838 0.756256 12.1803C0.656505 11.0762 0.5625 9.60942 0.5625 8C0.5625 6.39058 0.656505 4.92379 0.756257 3.81973C0.892104 2.31616 2.07738 1.15643 3.5755 1.03395ZM4.41663 4.93726C4.72729 4.93726 4.97913 5.1891 4.97913 5.49976V8.62476C4.97913 9.34963 5.56675 9.93726 6.29163 9.93726C7.0165 9.93726 7.60413 9.34963 7.60413 8.62476V5.49976C7.60413 5.1891 7.85597 4.93726 8.16663 4.93726C8.47729 4.93726 8.72913 5.1891 8.72913 5.49976V8.62476C8.72913 9.97095 7.63782 11.0623 6.29163 11.0623C4.94543 11.0623 3.85413 9.97095 3.85413 8.62476V5.49976C3.85413 5.1891 4.10597 4.93726 4.41663 4.93726ZM10.2501 4.93726C9.9394 4.93726 9.68756 5.1891 9.68756 5.49976V10.4998C9.68756 10.8104 9.9394 11.0623 10.2501 11.0623C10.5607 11.0623 10.8126 10.8104 10.8126 10.4998V9.60392H12.2292C13.5179 9.60392 14.5626 8.55925 14.5626 7.27059C14.5626 5.98193 13.5179 4.93726 12.2292 4.93726H10.2501ZM12.2292 8.47892H10.8126V6.06226H12.2292C12.8966 6.06226 13.4376 6.60325 13.4376 7.27059C13.4376 7.93793 12.8966 8.47892 12.2292 8.47892Z"></path></svg><p>${
-            data.name
-        }</p></a><p class="time" title="${numConverter.timeStamp(data.ctime).full}">${numConverter.timeStamp(data.ctime).mini}</p></div></div>
+        if (!GM_getValue("recommend_status")) {
+            return;
+        }
+        try {
+            let url = `https://www.bilibili.com${data.goto == "av" ? `/video/av${data.param}` : data.uri}`;
+            let text = `
+            <a href="${url}" class="video_content" target="_blank">
+            <div class="preview_pic">
+                <div class="preview_bar">
+                    <div class="bar_content"></div>
+                </div>
+            </div>
+            </div>
+            <img src="${data.cover}@672w_378h_1c.webp" alt="${data.title}">
+            <div class="video_mark">
+                <div class="video_mark_text"></div>
+            </div>
+            <div class="video_mask">
+                <div class="views">
+                    <svg t="1658670007102" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="18155" width="200" height="200">
+                        <path d="M800 128H224C134.4 128 64 198.4 64 288v448c0 89.6 70.4 160 160 160h576c89.6 0 160-70.4 160-160V288c0-89.6-70.4-160-160-160z m96 608c0 54.4-41.6 96-96 96H224c-54.4 0-96-41.6-96-96V288c0-54.4 41.6-96 96-96h576c54.4 0 96 41.6 96 96v448z" p-id="18156" fill="#ffffff"></path>
+                        <path d="M684.8 483.2l-256-112c-22.4-9.6-44.8 6.4-44.8 28.8v224c0 22.4 22.4 38.4 44.8 28.8l256-112c25.6-9.6 25.6-48 0-57.6z" p-id="18157" fill="#ffffff"></path>
+                    </svg>
+                    <p class="num"> ${numConverter.view(data.play)}</p>
+                    <svg t="1658672093240" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3299" width="200" height="200">
+                        <path d="M857.28 344.992h-264.832c12.576-44.256 18.944-83.584 18.944-118.208 0-78.56-71.808-153.792-140.544-143.808-60.608 8.8-89.536 59.904-89.536 125.536v59.296c0 76.064-58.208 140.928-132.224 148.064l-117.728-0.192A67.36 67.36 0 0 0 64 483.04V872c0 37.216 30.144 67.36 67.36 67.36h652.192a102.72 102.72 0 0 0 100.928-83.584l73.728-388.96a102.72 102.72 0 0 0-100.928-121.824zM128 872V483.04c0-1.856 1.504-3.36 3.36-3.36H208v395.68H131.36A3.36 3.36 0 0 1 128 872z m767.328-417.088l-73.728 388.96a38.72 38.72 0 0 1-38.048 31.488H272V476.864a213.312 213.312 0 0 0 173.312-209.088V208.512c0-37.568 12.064-58.912 34.72-62.176 27.04-3.936 67.36 38.336 67.36 80.48 0 37.312-9.504 84-28.864 139.712a32 32 0 0 0 30.24 42.496h308.512a38.72 38.72 0 0 1 38.048 45.888z" p-id="3300" fill="#ffffff"></path>
+                    </svg>
+                    <p class="num">${numConverter.view(data.like)}</p>
+                </div>
+                <div class="duration">${numConverter.duration(data.duration)}</div>
+            </div>
+        </a>
+        <div class="video_text">
+            <a class="video_title" href="${url}" target="_blank" title="${data.title}">${data.title}</a>
+            <div class="owner">
+                <a class="up_name" href="https://space.bilibili.com/${data.mid}" target="_blank">
+                    <svg width="18" height="16" viewBox="0 0 18 16" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M9 2.0625C6.85812 2.0625 4.98983 2.1725 3.67735 2.2798C2.77861 2.35327 2.08174 3.04067 2.00119 3.93221C1.90388 5.00924 1.8125 6.43727 1.8125 8C1.8125 9.56273 1.90388 10.9908 2.00119 12.0678C2.08174 12.9593 2.77861 13.6467 3.67735 13.7202C4.98983 13.8275 6.85812 13.9375 9 13.9375C11.1421 13.9375 13.0105 13.8275 14.323 13.7202C15.2216 13.6467 15.9184 12.9595 15.9989 12.0682C16.0962 10.9916 16.1875 9.56386 16.1875 8C16.1875 6.43614 16.0962 5.00837 15.9989 3.9318C15.9184 3.04049 15.2216 2.3533 14.323 2.27983C13.0105 2.17252 11.1421 2.0625 9 2.0625ZM3.5755 1.03395C4.9136 0.924562 6.81674 0.8125 9 0.8125C11.1835 0.8125 13.0868 0.924583 14.4249 1.03398C15.9228 1.15645 17.108 2.31588 17.2438 3.81931C17.3435 4.92296 17.4375 6.38948 17.4375 8C17.4375 9.61052 17.3435 11.077 17.2438 12.1807C17.108 13.6841 15.9228 14.8436 14.4249 14.966C13.0868 15.0754 11.1835 15.1875 9 15.1875C6.81674 15.1875 4.9136 15.0754 3.5755 14.966C2.07738 14.8436 0.892104 13.6838 0.756256 12.1803C0.656505 11.0762 0.5625 9.60942 0.5625 8C0.5625 6.39058 0.656505 4.92379 0.756257 3.81973C0.892104 2.31616 2.07738 1.15643 3.5755 1.03395ZM4.41663 4.93726C4.72729 4.93726 4.97913 5.1891 4.97913 5.49976V8.62476C4.97913 9.34963 5.56675 9.93726 6.29163 9.93726C7.0165 9.93726 7.60413 9.34963 7.60413 8.62476V5.49976C7.60413 5.1891 7.85597 4.93726 8.16663 4.93726C8.47729 4.93726 8.72913 5.1891 8.72913 5.49976V8.62476C8.72913 9.97095 7.63782 11.0623 6.29163 11.0623C4.94543 11.0623 3.85413 9.97095 3.85413 8.62476V5.49976C3.85413 5.1891 4.10597 4.93726 4.41663 4.93726ZM10.2501 4.93726C9.9394 4.93726 9.68756 5.1891 9.68756 5.49976V10.4998C9.68756 10.8104 9.9394 11.0623 10.2501 11.0623C10.5607 11.0623 10.8126 10.8104 10.8126 10.4998V9.60392H12.2292C13.5179 9.60392 14.5626 8.55925 14.5626 7.27059C14.5626 5.98193 13.5179 4.93726 12.2292 4.93726H10.2501ZM12.2292 8.47892H10.8126V6.06226H12.2292C12.8966 6.06226 13.4376 6.60325 13.4376 7.27059C13.4376 7.93793 12.8966 8.47892 12.2292 8.47892Z"></path>
+                    </svg>
+                    <p>${data.name}</p>
+                </a>
+                <p class="time" title="${numConverter.timeStamp(data.ctime).full}">${numConverter.timeStamp(data.ctime).mini}</p>
+            </div>
+        </div>
         `;
-        let dom = document.createElement("div");
-        dom.className = "recommend_content";
-        dom.innerHTML = text;
-        document.getElementById("dynamic_recommend").appendChild(dom);
-        // 悬浮预览
-        hoverPreview(dom, data.param);
-        // 添加稍后在看
-        addMarkVideo(dom.querySelector(".video_mark"), data.param);
+            let dom = document.createElement("div");
+            dom.className = "recommend_content";
+            dom.innerHTML = text;
+            document.getElementById("dynamic_recommend").appendChild(dom);
+            // 悬浮预览
+            hoverPreview(dom, data.param);
+            // 添加稍后在看
+            addMarkVideo(dom.querySelector(".video_mark"), data.param);
+
+            if (recommendData.length < 3) {
+                getRecommendData();
+            }
+        } catch (err) {}
     }
 
     function getRecommendData() {
@@ -360,29 +388,30 @@
 
     function scrollRefsh() {
         // 监听滚动实现自动添加推荐
+        let previous = 0;
+        const fillDom = function () {
+            if ((Date.now() - previous <= 500) | !GM_getValue("recommend_status")) {
+                return;
+            }
+            previous = Date.now();
+            const offsetBottom =
+                document.querySelector("aside.right").offsetHeight -
+                (document.querySelector("aside.right > section").offsetHeight + document.querySelector("#dynamic_recommend").offsetHeight);
+            const addNum = Math.round(offsetBottom / 260);
+            for (let index = 0; index < addNum; index++) {
+                addRecommend();
+            }
+        };
+
         let timer = setInterval(function () {
             if (recommendData[0] != undefined) {
-                for (let index = 0; index < 5; index++) {
-                    addRecommend();
-                }
+                fillDom();
+                addRecommend();
                 clearInterval(timer);
             }
         });
 
-        var lastAdd = Date.now();
-        window.onscroll = function () {
-            if (document.querySelector("div.recommend_switch > span").classList.contains("on")) {
-                let scrollPlace = document.body.scrollTop || document.documentElement.scrollTop;
-                let domHight = document.getElementById("dynamic_recommend").clientHeight - window.screen.availHeight;
-                if (recommendData[0] != undefined && scrollPlace >= domHight) {
-                    addRecommend();
-                }
-                if (recommendData.length < 2 && Date.now() - lastAdd >= 1000) {
-                    lastAdd = Date.now();
-                    getRecommendData();
-                }
-            }
-        };
+        window.onscroll = fillDom;
     }
 
     function addMarkVideo(markVideo, id) {
