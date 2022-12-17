34f70b63f78de0e0e751b66d307725e90f2987db
幻非 <eg1981400335@gmail.com>
2022-12-17 21:41:34 +0800

    限制图片最大宽度，去除顶部广告横幅，增加一键折叠键

--- zhihuSimplified/zhihuSimplified.user.js
+++ zhihuSimplified/zhihuSimplified.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎宽屏
 // @namespace    https://ixory.com
-// @version      1.0.2
+// @version      1.0.3
 // @description  将网页主体部分变宽，去除杂冗部分
 // @author       huanfei
 // @match        *://*.zhihu.com/*
@@ -13,7 +13,7 @@
 
 (function () {
     var url = window.location.href;
-    var style = '';
+    var style = '.origin_image{max-width:50% !important}';
 
     switch (url.split('.')[0].split('/')[2]) {
         case 'www':
@@ -61,7 +61,6 @@
             break;
     }
 
-    console.log(style);
     GM_addStyle(style);
 
     function homePage() {
@@ -70,6 +69,12 @@
         style += '.Topstory-container > div:nth-child(2){display:none}';
         style += '.css-yhjwoe{justify-content:space-between;}';
         style += '.css-yhjwoe > div{margin: 0}';
+        document.onreadystatechange = function () {
+            console.log("执行")
+            if (document.querySelector('.Topstory>div:not([class])')) {
+                document.querySelector('.Topstory>div:not([class]) svg').dispatchEvent(new MouseEvent('click', { bubbles: true }));
+            }
+        };
     }
 
     function searchPage() {
@@ -112,15 +117,39 @@
 
     function linkPage() {
         let trueURL = new URL(window.location.href).searchParams.get('target');
-        window.location.href = trueURL;
+        window.location.replace(trueURL);
     }
 
     // 去除外链限制
-    document.addEventListener('click', () => {
+    document.addEventListener('click', e => {
         document.querySelectorAll("a.external:not([class~='modify'])").forEach(e => {
             let trueURL = new URL(e.href).searchParams.get('target');
             e.href = trueURL;
             e.classList.add('modify');
         });
+        if (e.target.className == 'css-18hmmtu') {
+            document.querySelector('.css-19q29v6 .Button.css-1x9te0t').click();
+        }
     });
+
+    window.onload = () => {
+        GM_addStyle('.CornerAnimayedFlex{height:130px}.CornerAnimayedFlex--hidden{height:0}.Button.fold.css-gdd4kf{margin-top:10px;transform:rotate(90deg)}');
+        const foldButton = document.createElement('button');
+        foldButton.className = 'Button fold css-gdd4kf';
+        foldButton.setAttribute('aria-label', '全部折叠');
+        foldButton.setAttribute('data-tooltip', '全部折叠');
+        foldButton.setAttribute('data-tooltip-position', 'left');
+        foldButton.innerHTML =
+            '<svg width="24" height="24" viewBox="0 0 24 24" aria-hidden="true" class="Zi Zi--BackToTop" fill="currentColor"><path fill-rule="evenodd" d="M13.204 3.107a1.75 1.75 0 0 0-2.408 0L3.806 9.73c-1.148 1.088-.378 3.02 1.204 3.02h2.24V20c0 .966.784 1.75 1.75 1.75h6A1.75 1.75 0 0 0 16.75 20v-7.25h2.24c1.582 0 2.353-1.932 1.204-3.02l-6.99-6.623Z" clip-rule="evenodd"></path></svg>';
+        document.querySelector('#root > div > div:nth-child(6) > div > div').append(foldButton);
+        foldButton.addEventListener('click', () => {
+            document.querySelectorAll('span.RichContent-collapsedText').forEach(el => {
+                el.click();
+            });
+            document.querySelectorAll('.Zi.Zi--Comment.Button-zi').forEach(el => {
+                const button = el.parentElement.parentElement;
+                if (button.textContent == '​收起评论') button.click();
+            });
+        });
+    };
 })();
