9b3354646ce2a4e0c1d9d5ee1bb1548ef7a2fdce
幻非 <eg1981400335@gmail.com>
2023-08-16 23:32:50 +0800

    Update index.user.js

--- Bilibili/index.user.js
+++ Bilibili/index.user.js
@@ -22,7 +22,7 @@
         // 首页
         '.recommended-swipe.grid-anchor',
         '.feed-card:has(.bili-video-card__info--ad)',
-        '.floor-single-card',
+        '.container.is-version8 > .floor-single-card',
         '.bili-video-card.is-rcmd:has(.bili-video-card__info--ad)',
         '.feed-roll-btn',
         '.bili-live-card.is-rcmd',
@@ -37,20 +37,27 @@
         'div#activity_vote',
         'span.argue.item', // 安全提醒
         '.pop-live-small-mode', // 大家围观的直播
-        '.bili-vote', // 弹幕投票框
-        '.bili-score', // 弹幕评分框
-        '.bili-guide', // 引导三连 + 关注框
+        '.bpx-player-cmd-dm-wrap', // 弹幕弹窗
+        '.bili-cmd-shrink', // 弹幕弹窗
         // 动态页
         '.bili-dyn-item__interaction', // 热门评论
         '.bili-dyn-item__ornament', // 右上角标志
         '.bili-dyn-list__item:has(.bili-rich-text-module.lottery)', // 抽奖动态
         'aside.right .sticky', // 右侧推荐
         '.bili-dyn-content__dispute', // 安全提醒
+        '.b-avatar__canvas > div.b-avatar__layers:not(:nth-child(1))', // 头像挂件
+        '.b-avatar__layers > div:not(:nth-child(1))', // 头像挂件
         // 查找页
         '.video-list > div:has(.bili-video-card__info--ad)',
     ];
     GM_addStyle(style.map(e => `${e}{display:none !important;}`).join(''));
-    GM_addStyle(['.feed-card{margin-top: 40px !important;}', '.container.is-version8 > .bili-video-card.is-rcmd{margin-top: 40px !important;}'].join(''));
+    GM_addStyle(
+        [
+            '.feed-card{margin-top: 40px !important;}',
+            '.container.is-version8 > .bili-video-card.is-rcmd{margin-top: 40px !important;}',
+            '.b-avatar__layer.center{width: 48px !important;height: 48px !important;}',
+        ].join('')
+    );
     // 去除复制小尾巴
     window.addEventListener('copy', e => e.stopPropagation(), true);
 })();
